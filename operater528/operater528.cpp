7b334d5ba96ca7a4deec87af7576ec142e2d4c51
20172106639 <1757177667@qq.com>
2018-05-29 15:26:16 +0800

    改了好多次
    但还是有错误,正在思考中.

--- operater528/operater528.cpp
+++ operater528/operater528.cpp
@@ -9,19 +9,19 @@ class CFeet
 private:
 	int feet;
 	int inches;
+	int temp;
 public:
 	void setvalue(int fe, int in);
 	void display();
-	CFeet add(CFeet & objk);
-	/*Feet operator +(CFeet & objf);*/
-	Feet operator+(CFeet&objf);
+	CFeet add(CFeet & objf);
+	CFeet operator +(CFeet & objf);
+	CFeet operator-(CFeet&objf);
 };
-void CFeet::setvalue(int fe, int in)
+/*void CFeet::setvalue(int fe, int in)
 {
 	feet = fe + in / 12;
 	inches = in % 12;
-
-}
+	}
 void CFeet::display()
 {
 	cout << feet << "Ó¢³ß  " << inches << "Ó¢´ç " << endl;
@@ -37,13 +37,41 @@ CFeet CFeet::operator +(CFeet &objf)
 	CFeet temp;
 	temp.setvalue(feet + objf.feet, inches + objf.inches);
 	return temp;
+}*/
+void CFeet::setvalue(int fe, int in)
+{
+	feet = 12 * inches;
 }
-int main()
+if (feet < 0 && objf.feet < 0)
+{
+	temp = -(feet + objf.feet);
+} 
+else if (feet > objf.feet)
+{
+	{
+		temp = feet - objf.feet;
+	}
+else
+{
+	temp = objf.feet - feet;
+}
+}
+else if (feet > 0 && objf.feet > 0 && feet > objf.feet)
+{
+	{
+		temp = feet - objf.feet;
+	}
+else
+{
+	temp = objf.feet - feet;
+}
+}
+	int main()
 {
 	CFeet A, B, C;
-	A.setvalue(10, 11);
-	B.setvalue(6, 9);
-	C = A + B;
+	A.setvalue(1, 1);
+	B.setvalue(-1,-1);
+	C = A-B;
 	C.display();
 	return 0;
 }
